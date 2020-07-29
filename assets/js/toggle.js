7f2ff31fd679687de042847a27930b19b122e44d
Michelle Clement <michellelclement@gmail.com>
2020-07-29 10:04:21 +0000

    New little venice image added. JS files created and added including email contat form (which needs an error fixed) and toggle functions

---
+++ assets/js/toggle.js
@@ -0,0 +1,15 @@
+$('.show-more-btn').click(function(){
+	$(this).siblings("p").removeClass('invisible').toggle('slow');
+});
+
+/* $('#lighthouse-button').click(function(){
+	$('#lighthouse-p').removeClass('invisible').toggle('slow');
+});
+
+$('#venice-button').click(function(){
+	$('#venice-p').removeClass('invisible').toggle('slow');
+});
+
+$('#panagia-button').click(function(){
+	$('#panagia-p').removeClass('invisible').toggle('slow');
+}); */
