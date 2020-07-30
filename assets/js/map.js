7bed1dd86e1e6877077be642fee6460f057f76a5
Michelle Clement <michellelclement@gmail.com>
2020-07-30 08:57:35 +0000

    Google maps API added to site and working correactly. Markers to be added next

---
+++ assets/js/map.js
@@ -0,0 +1,9 @@
+function initMap() {
+var map = new google.maps.Map(document.getElementById('map'), {
+      zoom: 12, 
+      center: {
+          lat: 37.4467, 
+          lng: 25.3289,
+      },
+    });
+}
