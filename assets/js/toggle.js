cb8fd47fc9a0e6ad0798d83b72c0b75c9a50f786
Michelle Clement <michellelclement@gmail.com>
2020-07-30 07:46:19 +0000

    Added JS to change button text on toggle

--- assets/js/toggle.js
+++ assets/js/toggle.js
@@ -4,3 +4,11 @@ $(document).ready(function () {
 $(".show-more-btn").click(function () {
   $(this).siblings("p").removeClass('invisible').toggle("fast");
 });
+
+$(function(){
+   $(".show-more-btn").click(function () {
+      $(this).text(function(text){
+          return text === "Tell me less..." ? "Tell me more..." : "Tell me less...";
+      })
+   });
+})
