0bdc92e63978cf1e5b0598ca732d47f4f5c5ef77
Michelle Clement <michellelclement@gmail.com>
2020-07-31 09:09:47 +0000

    Added label with text to first marker

--- assets/js/map.js
+++ assets/js/map.js
@@ -7,10 +7,17 @@ function initMap() {
   var map = new google.maps.Map(document.getElementById("map"), options);
 
   var marker = new google.maps.Marker({
-        position:{lat:37.4445,lng:25.3254},
-        map:map,
-        icon:'https://developers.google.com/maps/documentation/javascript/examples/full/images/beachflag.png'
-      });
-        
-        
-        }
+    position: { lat: 37.4445, lng: 25.3254 },
+    map: map,
+    icon:
+      "https://developers.google.com/maps/documentation/javascript/examples/full/images/beachflag.png",
+  });
+
+  var infoWindow = new google.maps.InfoWindow({
+    content: "<h4>Mykonos Windmills<h4>",
+  });
+
+  marker.addListener("click", function () {
+    infoWindow.open(map, marker);
+  });
+}
