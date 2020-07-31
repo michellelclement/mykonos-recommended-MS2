55e71c348314a150a5be9f3133dfb56564ed2c44
Michelle Clement <michellelclement@gmail.com>
2020-07-31 09:22:33 +0000

    adding multiple markers but without the labels

--- assets/js/map.js
+++ assets/js/map.js
@@ -6,6 +6,7 @@ function initMap() {
 
   var map = new google.maps.Map(document.getElementById("map"), options);
 
+  /*
   var marker = new google.maps.Marker({
     position: { lat: 37.4445, lng: 25.3254 },
     map: map,
@@ -20,4 +21,22 @@ function initMap() {
   marker.addListener("click", function () {
     infoWindow.open(map, marker);
   });
+
+*/ 
+
+addMarker ({lat:37.4445,lng:25.3254});
+addMarker ({lat:37.4896,lng:25.3145});
+addMarker ({lat:37.4466,lng:25.3260});
+addMarker ({lat:37.4471,lng:25.3257});
+
+// Add marker function
+
+function addMarker(latlong){
+var marker = new google.maps.Marker({
+        position:latlong,
+        map:map,
+        icon:'https://developers.google.com/maps/documentation/javascript/examples/full/images/beachflag.png'
+      });
+
+}
 }
