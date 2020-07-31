86d3cfa3f9bd1fe071c996fc54bc947076b5f264
Michelle Clement <michellelclement@gmail.com>
2020-07-31 09:03:47 +0000

    added 1 marker with custom icon to test code

--- assets/js/map.js
+++ assets/js/map.js
@@ -5,4 +5,12 @@ function initMap() {
   };
 
   var map = new google.maps.Map(document.getElementById("map"), options);
-}
+
+  var marker = new google.maps.Marker({
+        position:{lat:37.4445,lng:25.3254},
+        map:map,
+        icon:'https://developers.google.com/maps/documentation/javascript/examples/full/images/beachflag.png'
+      });
+        
+        
+        }
